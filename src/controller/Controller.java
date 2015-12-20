59e13430fc66d66c59917edbdf975e42e329b422
Hopperexekution <jan.raupach@googlemail.com>
2015-12-20 02:01:14 +0100

    Klassen angepasst, Frame starbar gemacht, neue Buchung im Controller hinzugefügt, Buchungsliste wird nach jeder hinzugefügten Buchung nach Datum sortiert

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,11 @@
 package controller;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import model.Buchung;
 
 import view.Hauptmenue;
 
@@ -9,6 +14,7 @@ public class Controller {
 
 	//Objekt-Deklarationen
 	Hauptmenue hauptmenue;
+	List<Buchung> buchungsListe = new ArrayList<Buchung>();
 	
 	//Variablen-Deklarationen
 	
@@ -29,6 +35,12 @@ public class Controller {
 	
 	public Controller()
 	{
+		
+	}
+	public void buchungHinzufügen(int einheiten, String zugehoerigesLager, Date datum, boolean zubuchung){
+		Buchung buchung = new Buchung(einheiten, zugehoerigesLager, datum, zubuchung);
+		buchungsListe.add(buchung);
+		buchungsListe.sort(new DateComparator());
 		
 	}
 }
