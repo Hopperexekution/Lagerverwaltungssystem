d4b8e9fca63804359dae28a1c4eea7b236a54cdd
Hopperexekution <jan.raupach@googlemail.com>
2015-12-21 20:37:05 +0100

    Robins Klassen

--- src/view/ZulieferungsView.java
+++ src/view/ZulieferungsView.java
@@ -4,31 +4,53 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
+
+import controller.Controller;
+import model.Buchung;
+import model.Lager;
+import model.LagerModel;
+
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 
 public class ZulieferungsView extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
-	public ZulieferungsView() {
+	private Controller controller;
+
+	public ZulieferungsView(Controller controller/* , int gesamtMenge */) {
+		this.controller = controller;
 		getContentPane().setLayout(null);
-		
+
 		JScrollBar scrollBar = new JScrollBar();
 		scrollBar.setBounds(572, 162, 10, 121);
 		getContentPane().add(scrollBar);
-		
+
 		JLabel lblNeueZulieferung = new JLabel("Neue Zulieferung");
 		lblNeueZulieferung.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNeueZulieferung.setBounds(10, 11, 138, 14);
+		lblNeueZulieferung.setBounds(20, 11, 138, 20);
 		getContentPane().add(lblNeueZulieferung);
-		
+
 		JLabel lblGesamtmenge = new JLabel("Gesamtmenge");
 		lblGesamtmenge.setBounds(20, 45, 85, 14);
 		getContentPane().add(lblGesamtmenge);
-		
+
 		textField = new JTextField();
 		textField.addKeyListener(new KeyAdapter() {
 			@Override
@@ -39,75 +61,120 @@ public class ZulieferungsView extends JFrame {
 		textField.setBounds(121, 42, 86, 20);
 		getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblLager = new JLabel("Lager");
 		lblLager.setBounds(20, 90, 46, 14);
 		getContentPane().add(lblLager);
-		
-		JList list = new JList();
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Niedersachsen", "Frankreich"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		list.setBounds(20, 115, 55, 36);
-		getContentPane().add(list);
-		
+
+		Vector<Lager> auswaehlbareLager = new Vector<Lager>();
+		Lager wurzel = (Lager) controller.getLagerModel().getRoot();
+		if (!wurzel.equals(null)) {
+			findeBlaetter(wurzel, auswaehlbareLager);
+		} else if (wurzel.getChildList().isEmpty()) {
+			auswaehlbareLager.add(wurzel);
+		}
+		JComboBox<Lager> lagerAuswahl = new JComboBox<Lager>(auswaehlbareLager);
+		lagerAuswahl.setBounds(20, 115, 130, 20);
+		getContentPane().add(lagerAuswahl);
 		JLabel lblProzent = new JLabel("Prozent");
-		lblProzent.setBounds(126, 90, 46, 14);
+		lblProzent.setBounds(170, 90, 46, 14);
 		getContentPane().add(lblProzent);
-		
-		textField_1 = new JTextField();
-		textField_1.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				int key = e.getKeyCode();
-			}
-		});
-		textField_1.setBounds(121, 115, 86, 20);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
+		JTextField lblAngabeProzent = new JTextField();
+		lblAngabeProzent.setBounds(170, 115, 86, 20);
+		getContentPane().add(lblAngabeProzent);
+		lblAngabeProzent.setColumns(5);
+
 		JLabel lblEinheiten = new JLabel("Einheiten");
 		lblEinheiten.setBounds(367, 90, 55, 14);
 		getContentPane().add(lblEinheiten);
-		
-		JLabel label = new JLabel("0");
-		label.setEnabled(false);
-		label.setBounds(367, 116, 46, 14);
-		getContentPane().add(label);
-		
-		JList list_1 = new JList();
-		list_1.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Lager1", "Lager2"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
+		JLabel labelAnzahlEinheiten = new JLabel("0");
+		labelAnzahlEinheiten.setEnabled(false);
+		labelAnzahlEinheiten.setBounds(367, 116, 46, 14);
+		getContentPane().add(labelAnzahlEinheiten);
+
+		JList zulieferungLager = new JList();
+		DefaultListModel<Buchung> lieferungsBuchungen = new DefaultListModel<Buchung>();
+		zulieferungLager.setModel(lieferungsBuchungen);
+		JScrollPane scrollbarLieferung = new JScrollPane(zulieferungLager);
+		scrollbarLieferung.setBounds(20, 160, 550, 120);
+		getContentPane().add(scrollbarLieferung);
+
+		JButton butUndo = new JButton("Undo");
+		butUndo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
 			}
 		});
-		list_1.setBounds(20, 162, 562, 121);
-		getContentPane().add(list_1);
-		
-		JLabel lblUndo = new JLabel("Undo");
-		lblUndo.setBounds(29, 294, 46, 14);
-		getContentPane().add(lblUndo);
-		
-		JLabel lblRedo = new JLabel("Redo");
-		lblRedo.setBounds(121, 294, 46, 14);
-		getContentPane().add(lblRedo);
-		
-		JLabel lblBesttigen = new JLabel("Best\u00E4tigen");
-		lblBesttigen.setBounds(135, 338, 72, 14);
-		getContentPane().add(lblBesttigen);
-		
-		JLabel lblNchstesLager = new JLabel("N\u00E4chstes Lager");
-		lblNchstesLager.setBounds(29, 338, 96, 14);
-		getContentPane().add(lblNchstesLager);
+		butUndo.setBounds(20, 294, 90, 14);
+		getContentPane().add(butUndo);
+
+		JButton butRedo = new JButton("Redo");
+		butRedo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		butRedo.setBounds(130, 294, 90, 14);
+		getContentPane().add(butRedo);
+
+		JButton butBesttigen = new JButton("Best\u00E4tigen");
+		butBesttigen.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		butBesttigen.setBounds(440, 294, 100, 14);
+		getContentPane().add(butBesttigen);
+
+		JButton butNchstesLager = new JButton("N\u00E4chstes Lager");
+		butNchstesLager.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int einheiten;
+				if (lblAngabeProzent.equals("")) {
+					double prozent = Double.parseDouble(lblAngabeProzent.getText());
+					if (prozent > 100 || prozent <= 0) {
+						JOptionPane.showMessageDialog(null,
+								"Prozentangabe liegt entweder über 100% oder ist kleiner gleich 0%. Bitte andere Angabe eintragen.");
+					} else {
+						// einheiten = (int) (Math.floor((double)(gesamtMenge *
+						// (prozent/100))));
+						// Buchung neueBuchung = new
+						// Buchung(labelAnzahlEinheiten.get, zugehoerigesLager,
+						// datum, zubuchung)
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Es ist eine Prozentangabe für die Verteilung notwendig");
+				}
+
+			}
+		});
+		butNchstesLager.setBounds(240, 294, 180, 14);
+		getContentPane().add(butNchstesLager);
+
+		setBounds(400, 200, 630, 400);
+		setVisible(true);
+	}
+
+	private void findeBlaetter(Lager element, Vector<Lager> auswaehlbarLager) {
+		List<Lager> nachfolger = element.getChildList();
+		Iterator it = nachfolger.iterator();
+
+		while (it.hasNext()) {
+			Lager aktuelles = (Lager) it.next();
+			if (aktuelles.getChildList().isEmpty()) {
+				auswaehlbarLager.add(aktuelles);
+			}
+			findeBlaetter(aktuelles, auswaehlbarLager);
+		}
 	}
 }
